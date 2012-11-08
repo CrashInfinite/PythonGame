172c976b8b20070fae9eaee8b8f208f87bb3b7fd
Shaun Preston <shaunpreston1991@gmail.com>
2012-11-08 11:31:31 +0000

    added some things

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 class Character(object):
 
-    def __init__(self, name, race, level, health, stamina, defence, skill):
+    def __init__(self, name, race, level, health, stamina, defence, skill, status):
         self.name = name
         self.race = race
         self.level = level
@@ -8,16 +8,27 @@ class Character(object):
         self.stamina = stamina
         self.defence = defence
         self.skill = skill
+        self.status = status
 
     def level_up(self):
         level += 1
-        health = ((299*(level - 1))/39) + 1
-        stamina = ((99*(level - 1))/39) + 1
-        defense = ((99*(level - 1))/39) + 1
+        health += ((299*(level - 1))/39) + 1
+        stamina += ((99*(level - 1))/39) + 1
+        defense += ((99*(level - 1))/39) + 1
 
     @property
     def is_dead(self):
         return self.health == 0
+
+    @property
+    def is_tired(self):
+        return self.stamina == 0
+
+    @property
+    def is_poisoned(self):
+        return self.status = "poison"
+
+
 
 class Monster(Character):
     def __init__(self, name, race, level, health, stamina, defence, skill):
@@ -35,5 +46,6 @@ class Weapon(Item):
     def __init__(self, name, effect, requirements, damage):
         Item.__init__(self, name, effect, requirements)
         self.damage = damage
+
 
 
